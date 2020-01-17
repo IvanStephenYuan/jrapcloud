af60e18556a3077cf6147c9c078062f2cacef8a1
Ivan Stephen <yulong.yuan@jr-info.cn>
2020-01-18 00:00:43 +0800

    内容：初始版本 作者：Ivan Stephen 日期：20200117

---
+++ jrap-eureka/src/main/java/com/jrapcloud/eureka/JrapEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.jrapcloud.eureka;
+
+import com.jrapcloud.common.base.StartCommand;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrapEurekaApplication {
+    public static void main(String[] args) {
+        //使用自动接口
+        new StartCommand(args);
+        SpringApplication.run(JrapEurekaApplication.class, args);
+    }
+}
---
+++ jrap-eureka/src/test/java/com/jrapcloud/eureka/JrapEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrapcloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrapEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
