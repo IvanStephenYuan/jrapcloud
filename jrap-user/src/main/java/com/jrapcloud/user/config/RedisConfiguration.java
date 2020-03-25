aa461075d48f43bc0d1cd2f379d93506018fa56a
Ivan Stephen <yulong.yuan@jr-info.cn>
2020-03-26 07:57:10 +0800

    内容：增加Redis配置 作者：Ivan Stephen 日期：20200326

---
+++ jrap-user/src/main/java/com/jrapcloud/user/config/RedisConfiguration.java
@@ -0,0 +1,30 @@
+package com.jrapcloud.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @author IvanStephen
+ * @project jrapcloud
+ * @date 2020/3/26 7:29
+ **/
+@Configuration
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
